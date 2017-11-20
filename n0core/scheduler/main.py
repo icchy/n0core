408f8b08bc28bc754a203aa8980db21a804aa9f6
icchy <icchyr@gmail.com>
2017-11-20 14:47:44 +0900

    add scheduler

---
+++ n0core/scheduler/main.py
@@ -0,0 +1,25 @@
+try:
+    from n0core.lib.n0mq import N0MQ
+    from n0core.config import config
+except:
+    import sys
+    sys.path.append('../../')
+    from n0core.lib.n0mq import N0MQ
+    from n0core.config import config
+
+
+n0mq = N0MQ(config['pulsar']['service_url'])
+consumer = n0mq.subscribe(config['pulsar']['scheduler_topic'])
+
+
+@consumer.on('SomeRequest')
+def on_request(message):
+    pass
+
+
+def main():
+    n0mq.listen()
+
+
+if __name__ == '__main__':
+    main()
